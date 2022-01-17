85508db682b1417da00a21ca23ace83d36957949
Sebastian Cruz-Gonzalez <seba.2550@gmail.com>
2022-01-17 00:05:46 -0800

    Code for BFS

--- search/graph.py
+++ search/graph.py
@@ -21,7 +21,33 @@ class Graph:
         * If there is an end node and a path does not exist, return None
 
         """
-        return
+        visited_nodes = [] # List to keep track of the nodes we have visited already
+        queue = [] # List to use as a queue object for reading in nodes and their respective neighbors
+        backtrace = [] # List to keep track of our path
+        G = self.graph # Initialize the graph object
+
+        queue.append(start) # Add the start node to our queue object 
+        visited_nodes.append(start) # Add the start node to our list of nodes we have visited
+
+        while queue: # This loop continues until the queue is empty
+            current_node = queue.pop(0) # Remove the leftmost element from the queue, and establish it as the node we are currently looking at
+
+            if current_node == end:
+                return backtrace # There is an end node and a path from start node to end node. Return the path between them
+
+            for neighbor in G[current_node]: # Iterate over the neighboring nodes of the node we are currently visiting
+                if neighbor not in visited_nodes: # If the neighboring node has not been visited, add it to our list of visited nodes. Also enqueue it and keep track of it in our path
+                    visited_nodes.append(neighbor)
+                    queue.append(neighbor)
+                    backtrace[neighbor] = current_node # Record the parent node for backtracing through the traversed path
+
+        if end is None: # There is no end node. Return the order of traversal
+            return backtrace 
+
+        return None # There is an end node, but no path between the start and end. Return None
+
+        
+
 
 
 
