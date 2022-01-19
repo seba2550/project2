14f4d2bdb781da9e384bfcca7b60b10fd0010284
Sebastian Cruz-Gonzalez <seba.2550@gmail.com>
2022-01-19 14:47:25 -0800

    Update test_bfs.py

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -9,7 +9,7 @@ def test_bfs_traversal():
 
     '''
 
-    G = Graph("./data/Rosalind_Test_Data.adjlist")
+    G = graph("./data/Rosalind_Test_Data.adjlist")
     visited_nodes = G.bfs("2")
     data_nodes = ['2', '1', '4', '6', '3', '5'] # Nodes in the example graph from the Rosalind page
     assert visited_nodes == data_nodes # Assert that the returned list contains all nodes
@@ -17,7 +17,7 @@ def test_bfs_traversal():
 
 def test_bfs():
     
-    G = Graph("./data/Rosalind_Test_Data.adjlist")
+    G = graph("./data/Rosalind_Test_Data.adjlist")
     path = G.bfs("2", "6") # Start and end nodes exist, and there is a path between them
     one_true_path = ['2', '1', '4', '6'] # KKB reference, and also the actual path between 2 and 6 in the example graph from the Rosalind page
     assert path == one_true_path # Assert that the output path from my bfs method is the same as the path seen in the graph 
